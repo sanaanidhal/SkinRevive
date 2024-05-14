ff6cd19f9afd590f81265ae5b20162719e5afd87
sanaanidhal <nidhalsanaa11@supcom.tn>
2024-05-14 16:11:33 +0100

    Frontend work

---
+++ resources/views/index.blade.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <title>SkinRevive</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+
+    <!-- Additional CSS Files -->
+    <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
+
+    <style>
+        @import url('https://fonts.googleapis.com/css2?family=Teachers:ital,wght@0,400..800;1,400..800&display=swap');
+        @import url('https://fonts.googleapis.com/css2?family=Belleza&family=Italiana&display=swap');
+
+    </style>
+    <script src="https://kit.fontawesome.com/f5f77ab04f.js" crossorigin="anonymous"></script>
+</head>
+
+<body>
+    <!-- *****  Navbar  *****-->
+    <div id="navbar">
+        <div class='container'>
+            <div class="title">S k i n R e v i v e.</div>
+            <div class="nav">
+                <div><a href="#">News & Featured</a></div>
+                <div><a href="#">Best Seller</a></div>
+                <div><a href="#">Sale</a></div>
+                <div><a href="#">Store</a></div>
+            </div>
+            <div class="icons">
+                <div><i class="fa-solid fa-magnifying-glass"></i></div>
+                <div><i class="fa-regular fa-user"></i></div>
+                <div><i class="fa-solid fa-bag-shopping"></i></div>
+            </div>
+        </div>
+    </div>
+
+    <!-- *****  Hero section  ***** -->
+    <div id='hero'>
+        <div class="container">
+            <div class="content">
+                <div>
+                    <p class='top'>Science-Backed
+                        Skincare for
+                        Lasting Beauty</p>
+                </div>
+                <div class="filler"></div>
+                <div class="filler"></div>
+                <div style='width:30vw;'>
+                    <p class='mid'>
+                        We understand that the world of skincare can be
+                        overwhelming, with countless products and trends
+                        vying for your attention.</p>
+                    <div style='display:flex;justify-content:space-between;padding:0 5vw 0 0;'><a href="#"
+                            class='button'>SHOP NOW</a>
+                        <a href="#" class='button'>CONTACT US</a>
+                    </div>
+                </div>
+                <div></div>
+            </div>
+            <div class="background">
+              <div class='line'></div>
+
+            </div>
+
+        </div>
+    </div>
+
+
+
+
+    <!-- Scripts -->
+    <!-- Bootstrap core JavaScript -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+    <script src="assets/js/isotope.min.js"></script>
+    <script src="assets/js/owl-carousel.js"></script>
+    <script src="assets/js/counter.js"></script>
+    <script src="assets/js/custom.js"></script>
+
+</body>
+
+</html>
