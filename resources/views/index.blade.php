d7ccc2c4ba726dadf14f0003538d87e8cd153b71
sanaanidhal <nidhalsanaa11@supcom.tn>
2024-05-15 20:24:57 +0100

    frontend work

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -10,15 +10,14 @@
 
     <!-- Bootstrap core CSS -->
     <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@mdi/font@7.4.47/css/materialdesignicons.min.css">
 
 
     <!-- Additional CSS Files -->
     <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
 
     <style>
-        @import url('https://fonts.googleapis.com/css2?family=Teachers:ital,wght@0,400..800;1,400..800&display=swap');
-        @import url('https://fonts.googleapis.com/css2?family=Belleza&family=Italiana&display=swap');
-
+       @import url('https://fonts.googleapis.com/css2?family=Belleza&family=Cormorant+Garamond:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&family=Italiana&family=Roboto+Slab:wght@100..900&family=Slabo+27px&display=swap');
     </style>
     <script src="https://kit.fontawesome.com/f5f77ab04f.js" crossorigin="anonymous"></script>
 </head>
@@ -58,20 +57,102 @@
                         We understand that the world of skincare can be
                         overwhelming, with countless products and trends
                         vying for your attention.</p>
-                    <div style='display:flex;justify-content:space-between;padding:0 5vw 0 0;'><a href="#"
-                            class='button'>SHOP NOW</a>
+                    <div style='display:flex;justify-content:space-between;'>
+                        <a href="#"class='button'>SHOP NOW</a>
                         <a href="#" class='button'>CONTACT US</a>
                     </div>
                 </div>
-                <div></div>
             </div>
-            <div class="background">
-              <div class='line'></div>
+            <div class="feature-container">
+                <div class="feature">
+                    <span class="mdi mdi-creation"></span>
+                    <h2>Deeply Nourishing</h2>
+                    <p>Our baby shampoo is designed to cleanse your
+                        baby's hair and scalp without causing dryness.
+                    </p>
+                </div>
+                <hr style='border-color:#baba9f;'>
+                <div class="feature">
+                    <span class="mdi mdi-creation"></span>
+                    <h2>Hypoallergenic</h2>
+                    <p>Our baby shampoo and lotion are hypoallergenic
+                        and rigorously tested by dermatologists.</p>
+                </div>
+                <hr style='border-color:#baba9f;'>
+                <div class="feature">
+                    <span class="mdi mdi-creation"></span>
+                    <h2>Hydrating</h2>
+                    <p>Our baby lotion is enriched with natural ingredients
+                        Ike aloe vera and chamomile. </p>
+                </div>
+            </div>
 
+            <div class="background">
+                <div class='line'></div>
             </div>
 
         </div>
     </div>
+        <!-- *****  product section  ***** -->
+        <div id='product'>
+            <div class="container">
+                <div class="header">
+                    <div style='display:flex;'><div class="title">Beauty Care Product</div>
+                    <div style='color:#a2a2a2;font-size:30px;padding:7vh 0 0 0;'><span class="mdi mdi-creation"></span></div></div>
+                    <p>We understand that the world of skincare can be
+                        overwhelming, with countless products and trends.</p>
+                </div>
+                <div class="products">
+                    <a href="#"></a>
+                    <a style='' href="#"></a>
+                    <a href="#"></a>
+                    <a href="#"></a>
+                </div>
+                <div class="names">
+                    <div>Shapmoo + Wash</div>
+                    <div>Sunscreen SPF 50</div>
+                    <div>Baby Lotion</div>
+                    <div>Baby Balm</div>
+                </div>
+                <div class="prices">
+                    <div>$8.99</div>
+                    <div>$8.99</div>
+                    <div>$8.99</div>
+                    <div>$8.99</div>
+                </div>
+            </div>
+        </div>
+        <!-- *****  discount section  ***** -->
+        <div id='discount'>
+            <div class="container">
+                <div class="content">
+                    <div>
+                        <p class='top'>Made with the highest
+                            standards and safest
+                            ingredients.</p>
+                    </div>
+                    <div class="filler"></div>
+                    <div class="filler"></div>
+                    <div style='width:30vw;'>
+                        <p class='mid'>
+                            We understand that the world of skincare can be
+                            overwhelming, with countless products and trends
+                            vying for your attention.</p>
+                        <div style='display:flex;justify-content:space-between;'>
+                            <a href="#"class='button'>SHOP NOW</a>
+                            <a href="#" class='button'>CONTACT US</a>
+                        </div>
+                    </div>
+                </div>
+            
+    
+                <div class="background">
+                    <div class='line'></div>
+                </div>
+    
+            </div>
+        </div>
+
 
 
 
